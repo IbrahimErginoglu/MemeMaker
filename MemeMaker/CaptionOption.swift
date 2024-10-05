d3c7fe998e7b70200445619334c8a62b9a04ca37
ibrahimerginoglu <ibrahim.erginoglu@hisarschool.k12.tr>
2024-10-05 19:59:42 +0300

    Initial Commit

---
+++ MemeMaker/CaptionOption.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by ibrahim erginoglu on 3.10.2024.
+//
+
+import Foundation
+
+struct CaptionOption {
+    let emoji: String
+    let caption: String
+}
